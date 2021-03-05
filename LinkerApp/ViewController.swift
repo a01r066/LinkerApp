8bae6eed8ec0756485ab2628a47dbce0f72b855c
Thanh Vo Minh <thanhvom@imt-soft.com>
2021-03-05 22:50:38 +0700

    Initial Commit

---
+++ LinkerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkerApp
+//
+//  Created by Thanh Minh on 3/5/21.
+//  Copyright © 2021 Thanh Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
